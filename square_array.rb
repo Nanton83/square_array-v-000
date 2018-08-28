d22c7dea5314c6ad638c88354af9652047de2920
Nick Romans <nicholas.romans@gmail.com>
2018-08-28 00:41:48 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,13 @@
 def square_array(array)
+<<<<<<< HEAD
   squared = []
   array.each do |num|
 squared << num**2
 end
 squared
+=======
+  array.each do |squared|
+array << "#{squared}"*2
+end
+>>>>>>> 1ca7aed67e0972cdbf83f66c992232e6525f1660
 end
